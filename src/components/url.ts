44b49e4531ba7362f9276d57f9d7479bc37f4c51
Bong <2021bongwonny@gmail.com>
2024-01-09 23:05:32 +0900

    fix : resolving error of 'window is undefined'

--- src/components/url.ts
+++ src/components/url.ts
@@ -1,7 +1,5 @@
-const href = window?.location.href || 'http://localhost:3000';
-
-const STATUS_URL = href + 'api/status';
-const ALL_URL = href + 'api/all';
-const ORDER_URL = href + 'api/order';
+const STATUS_URL = 'api/status';
+const ALL_URL = 'api/all';
+const ORDER_URL = 'api/order';
 
 export { STATUS_URL, ALL_URL, ORDER_URL };

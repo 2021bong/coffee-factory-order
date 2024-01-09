77ad3c93ef1de4d830e33b1a752e2bb7c17c9ef6
Bong <2021bongwonny@gmail.com>
2024-01-09 21:51:44 +0900

    fix : modify url

--- src/components/url.ts
+++ src/components/url.ts
@@ -1,5 +1,7 @@
-const STATUS_URL = 'http://localhost:3000/api/status';
-const ALL_URL = 'http://localhost:3000/api/all';
-const ORDER_URL = 'http://localhost:3000/api/order';
+const href = window?.location.href || 'http://localhost:3000';
+
+const STATUS_URL = href + 'api/status';
+const ALL_URL = href + 'api/all';
+const ORDER_URL = href + 'api/order';
 
 export { STATUS_URL, ALL_URL, ORDER_URL };

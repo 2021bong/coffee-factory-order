ca7d86e084a70d77140cdfdec3d7a32612b43ed0
Bong <2021bongwonny@gmail.com>
2024-01-13 23:56:51 +0900

    feat : add meta og:image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import Head from 'next/head';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import './globals.css';
 import './reset.css';
@@ -37,6 +38,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
   return (
     <html lang='ko'>
+      <Head>
+        <meta
+          property='og:image'
+          content='https://github.com/2021bong/coffee-factory-order/assets/49029756/c89aa253-7d2f-494a-a435-c56349df12d0'
+        />
+      </Head>
       <body>
         <SpeedInsights />
         <ModalContext.Provider value={{ show, openModal, closeModal, orderableStatus, setDisableOrder }}>
